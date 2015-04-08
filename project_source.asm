64c45728af8ccc74b43c7273ed9bc4728837c35f
Owen Chiaventone <omc8db@mst.edu>
2015-04-08 17:55:32 -0500

    Increment, decrement in place. Decrement not yet checking for overflow

--- project_source.asm
+++ project_source.asm
@@ -1,16 +1,27 @@
 #include <reg932.h>
 #include <simon2.h>
 
+;These names come from simon2.h. They are printed on the silkscreen.
+;Inputs
 #define UP_SWITCH SW4
 #define DOWN_SWITCH SW1
 
+#define MALICK_BUTTON SW7
+#define OWEN_BUTTON SW8
+#define JUDAH_BUTTON SW9
+#define DRUE_BUTTON SW6
+#define EXIT_BUTTON SW3
+
+;Outputs
 #define LED_BIT_0 LED1_RED
 #define LED_BIT_1 LED2_AMB
 #define LED_BIT_2 LED4_YEL
 #define LED_BIT_3 LED5_RED
 
-#define DELAY_CYCLES 50	; Change this
+;Parameters
+#define DEBOUNCE_DELAY_CYCLES 255	; Change this
 
+;Reserved Registers
 #define COUNT_REGISTER R2
 
 cseg at 0			; tells the assembler to place the first
@@ -35,10 +46,27 @@ skip_dec:			;
 
 ;start of increment subroutine
 increment:			;
-	lcall delay		; Delay to debounce
-inc_no_release:			;
+	lcall debounce_delay		; Delay to debounce
+	inc_no_release:		;
 	jnb UP_SWITCH,inc_no_release;
-	setb LED_BIT_0		;
+
+	;Increment
+	inc COUNT_REGISTER	;
+	;update display
+	lcall led_display_count	;
+
+	;check for overflow
+	MOV A,COUNT_REGISTER	;
+	ANL A, #10h		; Check the fifth bit for overflow
+	jz inc_no_overflow	;
+	lcall buzz		;
+	mov COUNT_REGISTER,#0	; Reset the counter
+
+	
+	inc_no_overflow:	;
+	
+	;more debouncing
+	lcall debounce_delay	;
 	ret			;
 ;end of increment subroutine
 
@@ -46,10 +74,20 @@ inc_no_release:			;
 
 ;start of decrement subroutine
 decrement:			;
-	lcall delay		;
-dec_no_release:			;
+	lcall debounce_delay	;
+	dec_no_release:		;
 	jnb DOWN_SWITCH,dec_no_release;
 	clr LED_BIT_0		;
+
+	;Decrement
+	dec COUNT_REGISTER	;
+	;update display
+	lcall led_display_count	;
+
+	;check for overflow
+
+	;more debouncing
+	lcall debounce_delay	;
 	ret			;
 ;end of decrement subroutine
 
@@ -57,17 +95,66 @@ dec_no_release:			;
 
 ;start of buzz subroutine
 buzz:				;
+	cpl LED8_RED		;
 	ret			;
 ;end of buzz subroutine
 
 
 
-;start of delay subroutine
-delay:
-	mov R3,#DELAY_CYCLES	;
-delay_loop:			;
-	djnz R3,delay_loop	;
+;start of debounce_delay subroutine
+debounce_delay:
+	mov R4,#DEBOUNCE_DELAY_CYCLES	;
+debounce_delay_loop0:
+	mov R3,#DEBOUNCE_DELAY_CYCLES	;
+debounce_delay_loop1:		;
+	djnz R3,debounce_delay_loop1;
+	djnz R4,debounce_delay_loop0;
 	ret			;
-;end of delay subroutine
+;end of debounce_delay subroutine
 
+
+
+;start of led_display_count subroutine
+led_display_count:			;
+	mov A, COUNT_REGISTER	;
+	anl A, #01h		; //Get the lsb of count
+	;A is nonzero if the bit we want is nonzero
+	; Otherwise, A is the value of the bit
+	jz bit0_off_jmp	;  
+	clr LED_BIT_0		;
+	sjmp bit0_on_jmp	; 
+bit0_off_jmp:			;
+	setb LED_BIT_0		;
+bit0_on_jmp:			;
+
+	mov A, COUNT_REGISTER	;
+	anl A, #02h		; //Get the second bit of count
+	jz bit1_off_jmp	;  
+	clr LED_BIT_1		;
+	sjmp bit1_on_jmp	; 
+bit1_off_jmp:			;
+	setb LED_BIT_1		;
+bit1_on_jmp:			;
+
+	mov A, COUNT_REGISTER	;
+	anl A, #04h		; //Get the third bit of count
+	jz bit2_off_jmp	;  
+	clr LED_BIT_2		;
+	sjmp bit2_on_jmp	; 
+bit2_off_jmp:			;
+	setb LED_BIT_2		;
+bit2_on_jmp:			;
+	
+	mov A, COUNT_REGISTER	;
+	anl A, #08h		; //Get the fourth bit of count
+	jz bit3_off_jmp	;  
+	clr LED_BIT_3		;
+	sjmp bit3_on_jmp	; 
+bit3_off_jmp:			;
+	setb LED_BIT_3		;
+bit3_on_jmp:			;
+
+	ret			;
+;end of count_output subroutine
+   
 end
