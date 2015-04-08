1da885877ab402a3d21b3dcb9879ccecf733505e
Owen Chiaventone <omc8db@mst.edu>
2015-04-08 18:56:07 -0500

    Basic feature set done

--- project_source.asm
+++ project_source.asm
@@ -20,9 +20,11 @@
 
 ;Parameters
 #define DEBOUNCE_DELAY_CYCLES 255	; Change this
+#define BUZZ_DELAY_CYCLES
 
 ;Reserved Registers
 #define COUNT_REGISTER R2
+;R4-R7 available for other usage
 
 cseg at 0			; tells the assembler to place the first
 				; instruction at address 0
@@ -30,7 +32,7 @@ setup:				;
 	mov P2M1,#0		; set Port 2 to bi-directional
 	mov P1M1,#0		; set Port 1 to bi-directional
 	mov P0M1,#0		; set Port 0 to bi-directional
-
+	MOV TMOD,#0x01		; sets TIMER 0 into mode 1 operation 
 	mov COUNT_REGISTER,#0	; Initialize count to 0
 
 loop:				; label for the sjmp instruction
@@ -52,8 +54,6 @@ increment:			;
 
 	;Increment
 	inc COUNT_REGISTER	;
-	;update display
-	lcall led_display_count	;
 
 	;check for overflow
 	MOV A,COUNT_REGISTER	;
@@ -63,7 +63,9 @@ increment:			;
 	mov COUNT_REGISTER,#0	; Reset the counter to 0
 
 	
- inc_no_overflow:	;
+ inc_no_overflow:		;
+	;update display
+	lcall led_display_count	;
 	
 	;more debouncing
 	lcall debounce_delay	;
@@ -81,8 +83,6 @@ decrement:			;
 
 	;Decrement
 	dec COUNT_REGISTER	;
-	;update display
-	lcall led_display_count	;
 
 	;check for overflow
 	MOV A,COUNT_REGISTER	;
@@ -94,6 +94,8 @@ decrement:			;
 	
  dec_no_overflow:	;
 	
+	;update display
+	lcall led_display_count	;
 
 	;more debouncing
 	lcall debounce_delay	;
@@ -103,12 +105,26 @@ decrement:			;
 
 
 ;start of buzz subroutine
-buzz:				;
-	cpl LED8_RED		;
+buzz:	
+	mov R3, #128	;
+ buzzer_loop:	
+  	setb BUZZER	;	
+	lcall buzz_delay	;
+	clr BUZZER		;
+	lcall buzz_delay	;
+ djnz R3, buzzer_loop	;
 	ret			;
 ;end of buzz subroutine
 
-
+buzz_delay:
+	mov R5, #21			;
+ buzz_delay_outer:
+ 	mov R6, #255			;
+  buzz_delay_inner:				
+  djnz R6, buzz_delay_inner	;
+ djnz R5, buzz_delay_outer	;
+ 	ret				;
+	
 
 ;start of debounce_delay subroutine
 debounce_delay:
